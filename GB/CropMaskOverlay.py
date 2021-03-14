b0bac8657c12d00fb2fb509530e851e0bd4a7a4e
geobell9 <71470349+geobell9@users.noreply.github.com>
2021-03-14 16:28:07 -0400

    Add files via upload

--- GB/CropMaskOverlay.py
+++ GB/CropMaskOverlay.py
@@ -1,29 +1,37 @@
 import numpy as np
 from cv2 import cv2
 import os.path
+
         
  
-def CROPMASK(img,pts,clr) : 
+def CROPMASK(img,pts,lndmark,clr) : 
     
-    ##clr ranges from 1-255 1 being black and 255 being white for gray scale values
+    mydict = {
+        'bottomhalf': pts[0:16],
+        'eyebrows': pts[17:26],
+        'nose': pts[27:35],
+        'eyes': pts[36:47],
+        'lips': pts[48:60],
+        'topandnose': pts[17:35],
+        'tophalf': pts[17:47],
+        'tophalfconc': np.concatenate((pts[27:35],pts[17:26],pts[36:47])),
+        'all' : pts
+    }
     
-    ## (1) Crop the bounding rect
-    rect = cv2.boundingRect(pts)
-    x,y,w,h = rect
-    croped = img[y:y+h, x:x+w].copy()
-
-    ## (2) make mask
-    pts = pts - pts.min(axis=0)
-
-    mask = np.zeros(croped.shape[:2], np.uint8)
-    cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
-
-    ## (3) do bit-op
-    dst = cv2.bitwise_and(croped, croped, mask=mask)
-
-    ## (4) add the white background
-    bg = np.ones_like(croped, np.uint8)*clr
-    cv2.bitwise_not(bg,bg, mask=mask)
-    dst2 = bg+ dst
     
-    return  dst2
+    maskpts = mydict[lndmark]
+    
+    cv2.drawContours(img, [maskpts], -1, (255, 255, 255), -1, cv2.LINE_AA)
+    maskedimg = cv2.fillPoly(img ,mydict[lndmark], (0,0,clr) )
+
+    
+    # rect = cv2.boundingRect(maskpts)
+    # x,y,w,h = rect
+    # rectMask = img[y:y+h, x:x+w].copy()
+    # mask = np.zeros(rectMask.shape[:2], np.uint8)
+    # cv2.drawContours(img, mask, -1, (255, 255, 255), -1, cv2.LINE_AA)
+    
+
+    return maskedimg
+
+    
